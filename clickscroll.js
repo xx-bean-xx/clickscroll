07be0ff3a0364b634844677c56fb1bef219a1970
Leah Edwards <leah@thebean.dev>
2024-05-26 23:27:49 -0600

    better support for scrollable elements

--- clickscroll.js
+++ clickscroll.js
@@ -1,58 +1,97 @@
 
-// the page coords for where the scroll starts
-// https://developer.mozilla.org/en-US/docs/Web/CSS/CSSOM_view/Coordinate_systems#page
-var page = {x: -1, y: -1};
+// contains objects with a reference to scrollable elements under the cursor
+// and their initial scroll positions {element, x, y}
+var offsets = [];
 
-// used to prevent more than one click event listener from being bound
-var clickBlocked = false;
+// the viewport coordinates where the cursor started
+var viewport = {x: -1, y: -1};
+
+
+// this div is overlain on the viewport and captures all mouse events
+// so nothing can stop the propagation down the tree
+var overlay = document.createElement("div");
 
 function handleMousemove(ev) {
   ev.preventDefault();
+  ev.stopPropagation();
 
-  window.scroll({
-    top: page.y - ev.clientY,
-    left: page.x - ev.clientX
+  // pass scroll down to whatever element is under the cursor
+  offsets.forEach((offset) => {
+    offset.element.scrollTo({
+      top: offset.y + (viewport.y - ev.clientY),
+                            left: offset.x + (viewport.x - ev.clientX)
+    });
   });
-
-  // do not allow following links if you've just happened to click
-  // and drag the link
-  if (!clickBlocked) {
-    clickBlocked = true;
-    addEventListener("click", blockClick);
-  }
-}
-
-function blockClick(ev) {
-  ev.preventDefault();
 }
 
 function handleMousedown(ev) {
   ev.preventDefault();
+  ev.stopPropagation();
 
-  page.y = ev.pageY;
-  page.x = ev.pageX;
+  viewport.y = ev.clientY;
+  viewport.x = ev.clientX;
 
-  document.addEventListener("mousemove", handleMousemove);
+  // have to track all the elements you may need to scroll
+  offsets = [];
+  let scroll_candidates = document.elementsFromPoint(viewport.x, viewport.y);
+  scroll_candidates.forEach((el) => {
+    if ((el != overlay)
+      && (el.scrollHeight > el.clientHeight)) {
+      offsets.push({
+        element: el,
+        x: el.scrollLeft,
+        y: el.scrollTop
+      });
+      }
+  });
+
+  overlay.addEventListener("mousemove", handleMousemove);
 }
 
 function handleMouseup(ev) {
   ev.preventDefault();
+  ev.stopPropagation();
 
-  document.removeEventListener("mousemove", handleMousemove);
 
-  if (clickBlocked) {
-    // a little timeout so that if the click event fires after the mouseup event
-    // the click will still be blocked
-    setTimeout((_) => {
-      removeEventListener("click", blockClick);
-    }, 20);
-    clickBlocked = false;
+  overlay.removeEventListener("mousemove", handleMousemove);
+
+  // if the mouse moved less than ten pixels assume you meant to click
+  // and pass the event down to the next element at this position
+  if ( ( Math.abs(ev.clientX - viewport.x) < 10 )
+    && ( Math.abs(ev.clientY - viewport.y) < 10 ) )
+  {
+    let click_candidates = document.elementsFromPoint(ev.clientX, ev.clientY);
+    click_candidates.every((el) => {
+      if (el == overlay || el.click == undefined) {
+        return true;
+      }
+      el.click();
+      return false;
+    });
   }
 }
 
 function main () {
-  document.addEventListener("mousedown", handleMousedown, {capture: true});
-  document.addEventListener("mouseup", handleMouseup);
+  // styling to place the overlay as an overlay
+  overlay.id = "clickscroll-overlay";
+  overlay.style.width = "100vw";
+  overlay.style.height = "100vh";
+  overlay.style.background = "transparent";
+  overlay.style.position = "fixed";
+  overlay.style.left = "0";
+  overlay.style.top = "0";
+  overlay.style.zIndex = "9999";
+
+
+  // add all the click and drag handlers to the overlay
+  overlay.addEventListener("mousedown", handleMousedown, {capture: true});
+  overlay.addEventListener("mouseup", handleMouseup);
+  overlay.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    ev.stopPropagation();
+  });
+
+  document.body.appendChild(overlay);
 }
 
 main();
